2721a3c7de4d5fedf3037309156784f4816f27c9
Tran Hoang Hiep <22010389@st.phenikaa-uni.edu.vn>
2026-01-19 00:12:44 +0700

    Config MUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 
 
 function App() {
   return (
     <>
       <div>THH</div>
+      <Typography variant="body2" color="text.secondary" >Test Typography</Typography>
+
+
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
